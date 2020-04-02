7a32033794202ec508ccd4c7608e128e75427e99
Danny Noonan <dnoonan@cern.ch>
2020-04-02 18:23:26 -0500

    make setup work for csh as well

--- envSetup.sh
+++ envSetup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 NAME=hgcalPythonEnv
-LCG=/cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-centos7-gcc8-opt/setup.sh
+LCG=/cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-centos7-gcc8-opt
 
-source $LCG
+source $LCG/setup.sh
 # following https://aarongorka.com/blog/portable-virtualenv/, an alternative is https://github.com/pantsbuild/pex
 python -m venv --copies $NAME
 source $NAME/bin/activate
@@ -12,6 +12,7 @@ python -m pip install pyjet
 
 sed -i '40s/.*/VIRTUAL_ENV="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}" )")" \&\& pwd)"/' $NAME/bin/activate
 sed -i '1s/#!.*python$/#!\/usr\/bin\/env python/' $NAME/bin/*
-sed -i "2a source ${LCG}" $NAME/bin/activate
+sed -i "2a source ${LCG}/setup.sh" $NAME/bin/activate
+sed -i "4a source ${LCG}/setup.csh" $NAME/bin/activate.csh
 
 tar -zcf ${NAME}.tar.gz ${NAME}
